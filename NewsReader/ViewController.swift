3853c77e58c315c38d1e13bdf7851e93ae0f5161
karur4n <karuran.f@gmail.com>
2015-04-30 13:21:57 +0900

    Initial Commit

---
+++ NewsReader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NewsReader
+//
+//  Created by karur4n on 2015/04/30.
+//  Copyright (c) 2015年 karur4n. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
