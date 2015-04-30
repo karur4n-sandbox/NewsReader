3578f027619e44fda0c62dbd7e6546fbafd14a26
karur4n <karuran.f@gmail.com>
2015-04-30 14:02:18 +0900

    JSON データのパース

--- NewsReader/ViewController.swift
+++ NewsReader/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 import Alamofire
 
 class ViewController: UIViewController {
+    
+    var newsDataArray = NSArray()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,9 +20,15 @@ class ViewController: UIViewController {
         let requestUrl = "https://ajax.googleapis.com/ajax/services/search/news?v=1.0&topic=p&hl=ja&rsz=8"
         
         // web サーバーに対して HTTP 通信のリクエストを出してデータを取得
-        Alamofire.request(.GET, requestUrl).responseJSON {
-            (request, response, json, error) in
-            println(json)
+        Alamofire.request(.GET, requestUrl).responseJSON {(request, response, json, error) in
+            // まず JSON データを NSDictionary 型に変換
+            let jsonDic = json as! NSDictionary
+            // 辞書化した jsonDic からキー値 "responseData" を取り出す
+            let responseData = jsonDic["responseData"] as! NSDictionary
+            // reponseData からキー値"results"を取り出す
+            self.newsDataArray = responseData["results"] as! NSArray
+            println("\(self.newsDataArray)")
+            
         }
         
         // ニュース記事データをテーブルビューに表示
