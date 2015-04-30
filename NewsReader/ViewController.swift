822d9d47d16b74d6c4f14d0734dcbad2f24bcb9d
karur4n <karuran.f@gmail.com>
2015-04-30 15:18:21 +0900

    ニュース一覧を作成

--- NewsReader/ViewController.swift
+++ NewsReader/ViewController.swift
@@ -9,12 +9,17 @@
 import UIKit
 import Alamofire
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     var newsDataArray = NSArray()
 
+    @IBOutlet var table :UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        table.dataSource = self
+        table.delegate = self
         
         // ニュース情報の取得先
         let requestUrl = "https://ajax.googleapis.com/ajax/services/search/news?v=1.0&topic=p&hl=ja&rsz=8"
@@ -27,11 +32,45 @@ class ViewController: UIViewController {
             let responseData = jsonDic["responseData"] as! NSDictionary
             // reponseData からキー値"results"を取り出す
             self.newsDataArray = responseData["results"] as! NSArray
-            println("\(self.newsDataArray)")
+            // ニュース記事データをテーブルビューに表示
+            self.table.reloadData()
             
         }
+    }
+    
+    // テーブルビューのセルの数を newsDataArray に格納しているデータの数で設定
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return newsDataArray.count
+    }
+    
+    // セルに表示する内容を設定
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        // StoryBoard で取得した Cell を取得
+        let cell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "Cell")
+        // ニュース記事データを取得
+        var newsDic = newsDataArray[indexPath.row] as! NSDictionary
         
-        // ニュース記事データをテーブルビューに表示
+        // タイトルとタイトルの行数、公開日時を Cell にセット
+        cell.textLabel?.text = newsDic["title"] as? String
+        cell.textLabel?.numberOfLines = 3
+        cell.detailTextLabel?.text = newsDic["publishedDate"] as? String
+        return cell
+    }
+    
+    // テーブルビューのセルがタップされた処理
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        // セルのインデックスパス番号を出力
+        println("タップされたセルのインデックスパス:\(indexPath.row)")
+        // ニュース記事データを取得
+        var newsDic = newsDataArray[indexPath.row] as! NSDictionary
+        // ニュース記事の URL を取得
+        let newsUrl = newsDic["unescapedUrl"] as! String
+        // String を NSURL に変換
+        let url = NSURL(string: newsUrl)
+        // UIApplication インスタンスを作成
+        let app = UIApplication.sharedApplication()
+        // openURL メソッドで URL を引数に WEB ブラウザ Safari を起動
+        app.openURL(url!)
     }
 
     override func didReceiveMemoryWarning() {
---
+++ Pods/Target Support Files/Pods-NewsReader-Alamofire/Pods-NewsReader-Alamofire-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_NewsReader_Alamofire : NSObject
+@end
+@implementation PodsDummy_Pods_NewsReader_Alamofire
+@end
